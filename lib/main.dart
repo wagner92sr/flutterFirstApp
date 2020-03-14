5a1343ae14a6ecba3241fed9d35a13132b7a44fa
Wagner Rodrigues <wagnersr92@gmail.com>
2020-03-13 23:53:45 -0300

    Map para exibir as opcoes de resposta

--- lib/main.dart
+++ lib/main.dart
@@ -5,43 +5,52 @@ import './resposta.dart';
 
 main() => runApp(new PerguntaApp());
 
-class _PerguntaAppState extends State<PerguntaApp>{
+class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
 
-  void _responder(){
+  void _responder() {
     setState(() {
-      _perguntaSelecionada ++;      
+      _perguntaSelecionada++;
     });
   }
 
   @override
-  Widget build(BuildContext context){
-
-    final List<String> perguntas = [//List<String> nao eh necessario por causa da inferencia de tipo 
-      'Qual é o seu nome ?',
-      'Qual é a sua cor favorita ?'
+  Widget build(BuildContext context) {
+    final List<Map<String, Object>> perguntas = [
+      //List<String> nao eh necessario por causa da inferencia de tipo
+      {
+        'texto': 'Qual é a sua cor favorita ?',
+        'respostas': ['Amarelo', 'Verde', 'Preto', 'Branco'],
+      },
+      {
+        'texto': 'Qual é o seu animal favorito ?',
+        'respostas': ['Cachorro', 'Gato', 'Papagaio', 'Elefante'],
+      },
+      {
+        'texto': 'Qual é o seu time ?',
+        'respostas': ['Inter', 'Corinthians', 'Grêmio', 'Flamengo'],
+      },
     ];
 
+    List<String> respostas = perguntas[_perguntaSelecionada]['respostas'];
+
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Perguntas'),
-        ),
-        body: Column(
-          children: <Widget>[          
-            Questao(perguntas[_perguntaSelecionada]),  
-            Resposta('Resposta 1', _responder),
-            Resposta('Resposta 2', _responder),
-            Resposta('Resposta 3', _responder)
-          ],
-        ),
-      )
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Perguntas'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Questao(perguntas[_perguntaSelecionada]['texto']),
+          ...respostas.map((t) => Resposta(t, _responder)).toList(),
+        ],
+      ),
+    ));
   }
 }
 
-class PerguntaApp extends StatefulWidget{
-  _PerguntaAppState createState(){
+class PerguntaApp extends StatefulWidget {
+  _PerguntaAppState createState() {
     return _PerguntaAppState();
   }
 }
